405ea7457587898452def236ce401c0dc134d31a
조익현/엔마스터서비스팀/ND <ikhyeon.cho@nondooray.com>
2025-11-25 08:52:07 +0900

    feat. toolbar css 적용

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link>
-    <router-link to="/news">News</router-link>
-    <router-link to="/ask">Ask</router-link>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
     <router-link to="/jobs">Jobs</router-link>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
 
-<style></style>
+.header .router-link-exact-active {
+  color: #35495e;
+}
+
+/* 
+.header a는 .header 요소 안에 있는 <a> 요소에 적용됩니다.
+<router-link>는 실제로 <a> 태그로 렌더링되므로,
+.header a 셀렉터의 css는 툴바 내부의 모든 링크(<router-link>로 생성된 <a>)에 적용됩니다.
+*/
+.header a {
+  color: white;
+}
+</style>
