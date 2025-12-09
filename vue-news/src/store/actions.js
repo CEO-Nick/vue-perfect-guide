56e39bf3710b4b6142c8e0fd5dddd567c2e06028
nick <andantej281@gmail.com>
2025-12-09 09:05:12 +0900

    refactor. store 모듈화

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,21 @@
+import {fetchAskList, fetchJobsList, fetchNewsList} from "@/api";
+
+export default {
+  FETCH_NEWS(context) {
+    fetchNewsList()
+    .then((response) => context.commit('SET_NEWS', response.data))
+    .catch((error) => console.log(error));
+  },
+  // context 안에 commit이 있어서 바로 꺼내서 쓴다 = destructuring
+  FETCH_JOBS({commit}) {
+    // response 객체를 destructuring 가능
+    fetchJobsList()
+    .then(({data}) => commit('SET_JOBS', data))
+    .catch((error) => console.log(error));
+  },
+  FETCH_ASK({commit}) {
+    fetchAskList()
+    .then(({data}) => commit('SET_ASK', data))
+    .catch((error) => console.log(error));
+  },
+}
--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import {fetchAskList, fetchJobsList, fetchNewsList} from '../api/index';
+import mutations  from "@/store/mutations";
+import actions from "@/store/actions";
 
 Vue.use(Vuex);
 
@@ -21,34 +22,6 @@ export const store = new Vuex.Store({
       return state.jobs
     }
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-      .then((response) => context.commit('SET_NEWS', response.data))
-      .catch((error) => console.log(error));
-    },
-    // context 안에 commit이 있어서 바로 꺼내서 쓴다 = destructuring
-    FETCH_JOBS({commit}) {
-      // response 객체를 destructuring 가능
-      fetchJobsList()
-      .then(({data}) => commit('SET_JOBS', data))
-      .catch((error) => console.log(error));
-    },
-    FETCH_ASK({commit}) {
-      fetchAskList()
-      .then(({data}) => commit('SET_ASK', data))
-      .catch((error) => console.log(error));
-    },
-  },
+  mutations,
+  actions,
 });
