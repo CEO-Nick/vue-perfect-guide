84413e557b263da868ea8cbed7d5fd9704302b45
nick <andantej281@gmail.com>
2025-12-11 08:55:26 +0900

    feat. CommentItem 뷰 개선

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
-    <p>title: {{ itemInfo.title}}</p>
-    <p>user: {{ itemInfo.user}}</p>
-    <p>content: {{ itemInfo.content}}</p>
+    <section>
+    <!-- 질문 상세 정보-->
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${itemInfo.user}`">
+            {{ itemInfo.user }}
+          </router-link>
+          <div class="time">
+            {{ itemInfo.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2> {{ itemInfo.title }}</h2>
+    </section>
+
+    <section>
+      <!-- 질문 댓글 -->
+      <div v-html="itemInfo.content"/>
+    </section>
 
     <p> comments ({{ itemInfo.comments_count }})</p>
     <p v-for="comment in itemInfo.comments" :key="comment.id">
@@ -27,3 +46,20 @@
     },
   }
 </script>
+
+<style scoped>
+.user-container{
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user{
+  font-size: 2.5rem;
+}
+.user-description{
+  padding-left: 8px;
+}
+.time{
+  font-size: 0.7rem;
+}
+</style>
