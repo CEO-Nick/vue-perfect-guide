a5009d7de63a4765acbe2dc73ea5d48819d84100
nick <andantej281@gmail.com>
2025-12-11 09:01:17 +0900

    feat. Transition 적용

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="routing-fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -16,5 +18,13 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* Router Transition */
+.routing-fade-enter-active, .routing-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.routing-fade-enter, .routing-fade-leave-to
+  /* .routing-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
